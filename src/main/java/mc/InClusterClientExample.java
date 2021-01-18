9a7cd5838f292d16751def5ddf6fd881e4b0a411
Macoredroid <coredroid0401@gmail.com>
2021-01-18 04:39:51 +0000

    update

--- src/main/java/mc/InClusterClientExample.java
+++ src/main/java/mc/InClusterClientExample.java
@@ -10,7 +10,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
+package mc;
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.Configuration;
