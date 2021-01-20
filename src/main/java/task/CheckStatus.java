74f9be8fc2d54131bd0210c626e76b8685cb0051
macoredroid <coredroid0401@gmail.com>
2021-01-20 16:38:40 +0800

    update

--- src/main/java/task/CheckStatus.java
+++ src/main/java/task/CheckStatus.java
@@ -3,6 +3,7 @@ package task;
 import io.kubernetes.client.openapi.apis.CoreV1Api;
 import io.kubernetes.client.openapi.models.V1Pod;
 import io.kubernetes.client.openapi.models.V1PodList;
+import lombok.SneakyThrows;
 import mc.DTO.NodeInfo;
 import mc.DTO.PodInfo;
 import mc.DTO.ServiceInfo;
@@ -26,6 +27,7 @@ public class CheckStatus extends TimerTask {
         this.serviceNameMap=serviceNameMap;
         this.nodeMap=nodeMap;
     }
+    @SneakyThrows
     @Override
     public void run() {
         V1PodList list = null;
@@ -52,13 +54,9 @@ public class CheckStatus extends TimerTask {
             NodeInfo nodeInfo=entry.getValue();
             String nodeIP=entry.getKey();
             String command="curl http://"+nodeIP+":9100/metrics | grep 'node_memory_MemTotal_bytes\\|node_memory_MemAvailable_bytes\\|node_load1'";
-            final Process p;
-            try {
-                p = Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", command});
-            }catch (Exception e)
-            {
-                e.printStackTrace();
-            }
+
+            final Process p = Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", command});
+
             new Thread(() -> {
                 BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
                 String line;
