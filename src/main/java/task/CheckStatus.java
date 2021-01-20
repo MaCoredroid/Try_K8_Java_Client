e9517d0251f6096a26f38758eadc79bf4c792d39
macoredroid <coredroid0401@gmail.com>
2021-01-20 16:36:39 +0800

    update

--- src/main/java/task/CheckStatus.java
+++ src/main/java/task/CheckStatus.java
@@ -52,8 +52,9 @@ public class CheckStatus extends TimerTask {
             NodeInfo nodeInfo=entry.getValue();
             String nodeIP=entry.getKey();
             String command="curl http://"+nodeIP+":9100/metrics | grep 'node_memory_MemTotal_bytes\\|node_memory_MemAvailable_bytes\\|node_load1'";
+            final Process p;
             try {
-                final Process p = Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", command});
+                p = Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", command});
             }catch (Exception e)
             {
                 e.printStackTrace();
