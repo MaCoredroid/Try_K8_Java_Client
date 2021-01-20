8d6553860e5715d194986f63161fdaeed744b20d
macoredroid <coredroid0401@gmail.com>
2021-01-20 16:10:37 +0800

    update

--- src/main/java/mc/InClusterClientExample.java
+++ src/main/java/mc/InClusterClientExample.java
@@ -81,61 +81,57 @@ public class InClusterClientExample {
             nodeInfo.setNode_cpu_total(Objects.requireNonNull(Objects.requireNonNull(node.getStatus()).getCapacity()).get("cpu").getNumber().doubleValue());
             nodeMap.put(nodeIP,nodeInfo);
         }
-        V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
-        for (V1Pod item : list.getItems()) {
-            if (Objects.equals(Objects.requireNonNull(item.getMetadata()).getNamespace(), "default")) {
-                String[] arrOfStr = Objects.requireNonNull(Objects.requireNonNull(item.getMetadata()).getName()).split("-", 2);
-                String serviceName=arrOfStr[0];
-                if(!serviceNameMap.containsKey(serviceName))
-                {
-                    continue;
+        while(true) {
+            V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
+            for (V1Pod item : list.getItems()) {
+                if (Objects.equals(Objects.requireNonNull(item.getMetadata()).getNamespace(), "default")) {
+                    String[] arrOfStr = Objects.requireNonNull(Objects.requireNonNull(item.getMetadata()).getName()).split("-", 2);
+                    String serviceName = arrOfStr[0];
+                    if (!serviceNameMap.containsKey(serviceName)) {
+                        continue;
+                    }
+                    serviceNameMap.get(serviceName).getPodInfos().add(new PodInfo(Objects.requireNonNull(item.getMetadata()).getName(), Objects.requireNonNull(item.getStatus()).getPodIP(), Objects.requireNonNull(item.getStatus()).getHostIP()));
                 }
-                serviceNameMap.get(serviceName).getPodInfos().add(new PodInfo(Objects.requireNonNull(item.getMetadata()).getName(),Objects.requireNonNull(item.getStatus()).getPodIP(),Objects.requireNonNull(item.getStatus()).getHostIP()));
+
             }
+            System.out.println(serviceNameMap);
+            for (Map.Entry<String, NodeInfo> entry : nodeMap.entrySet()) {
+                NodeInfo nodeInfo = entry.getValue();
+                String nodeIP = entry.getKey();
+                String command = "curl http://" + nodeIP + ":9100/metrics | grep 'node_memory_MemTotal_bytes\\|node_memory_MemAvailable_bytes\\|node_load1'";
+                final Process p = Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", command});
+                new Thread(() -> {
+                    BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                    String line;
 
-        }
-        System.out.println(serviceNameMap);
-        for(Map.Entry<String, NodeInfo> entry : nodeMap.entrySet()) {
-            NodeInfo nodeInfo=entry.getValue();
-            String nodeIP=entry.getKey();
-            String command="curl http://"+nodeIP+":9100/metrics | grep 'node_memory_MemTotal_bytes\\|node_memory_MemAvailable_bytes\\|node_load1'";
-            final Process p = Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", command});
-            new Thread(() -> {
-                BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                String line;
-
-                try {
-                    while ((line = input.readLine()) != null) {
-                        if(!line.startsWith("#")) {
-                            if(line.startsWith("node_memory_MemTotal_bytes"))
-                            {
-                                Double value= Double.parseDouble(line.replace("node_memory_MemTotal_bytes","").trim());
-                                nodeInfo.setNode_memory_MemTotal_bytes(value);
-                            }
-                            else if(line.startsWith("node_memory_MemAvailable_bytes"))
-                            {
-                                Double value= Double.parseDouble(line.replace("node_memory_MemAvailable_bytes","").trim());
-                                nodeInfo.setNode_memory_MemAvailable_bytes(value);
-                            }
-                            else if(line.startsWith("node_load1")&&!line.startsWith("node_load15"))
-                            {
-                                Double value= Double.parseDouble(line.replace("node_load1","").trim());
-                                nodeInfo.setNode_load_1m(value);
+                    try {
+                        while ((line = input.readLine()) != null) {
+                            if (!line.startsWith("#")) {
+                                if (line.startsWith("node_memory_MemTotal_bytes")) {
+                                    Double value = Double.parseDouble(line.replace("node_memory_MemTotal_bytes", "").trim());
+                                    nodeInfo.setNode_memory_MemTotal_bytes(value);
+                                } else if (line.startsWith("node_memory_MemAvailable_bytes")) {
+                                    Double value = Double.parseDouble(line.replace("node_memory_MemAvailable_bytes", "").trim());
+                                    nodeInfo.setNode_memory_MemAvailable_bytes(value);
+                                } else if (line.startsWith("node_load1") && !line.startsWith("node_load15")) {
+                                    Double value = Double.parseDouble(line.replace("node_load1", "").trim());
+                                    nodeInfo.setNode_load_1m(value);
+                                }
                             }
                         }
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }).start();
-            try {
-                p.waitFor();
-            } catch (Exception ignored) {
+                }).start();
+                try {
+                    p.waitFor();
+                } catch (Exception ignored) {
 
+                }
+                nodeMap.put(nodeIP, nodeInfo);
             }
-            nodeMap.put(nodeIP,nodeInfo);
+            System.out.println(nodeMap);
         }
-        System.out.println(nodeMap);
 
         
     }
