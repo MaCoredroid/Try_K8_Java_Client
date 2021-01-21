58cf09851f5cf1e10eb2bf197afa56b0845e7795
macoredroid <coredroid0401@gmail.com>
2021-01-21 21:54:14 +0800

    update

--- src/main/java/task/Calculate.java
+++ src/main/java/task/Calculate.java
@@ -22,15 +22,16 @@ public class Calculate extends TimerTask {
         for(Map.Entry<String, PodInfo> entry:serviceNameMap.getOrDefault("application", new ServiceInfo()).getPods().entrySet())
         {
             String nodeIP=entry.getValue().getNodeIP();
-//            System.out.println(entry.getValue().getPodName()+"\n");
+            System.out.println(entry.getValue().getPodName()+"\n");
             if(nodeMap.containsKey(nodeIP))
             {
-//                NodeInfo nodeInfo=nodeMap.get(nodeIP);
-//                System.out.println("Node "+nodeInfo.getNodeName());
-//                System.out.println("Percents  "+entry.getValue().getCpu()/nodeInfo.getNode_top_cpu_value()+"  ");
-//                System.out.println("Estimate  "+(nodeInfo.getNode_cpu_total()-nodeInfo.getNode_top_cpu_value())/entry.getValue().getCpu()+"  ");
-//                System.out.println("NowNode  "+nodeInfo.getNode_top_cpu_percents()+"  ");
-//                System.out.println("NowNode  "+nodeInfo.getNode_load_cpu_percents()+"  ");
+                NodeInfo nodeInfo=nodeMap.get(nodeIP);
+                System.out.println("Node "+nodeInfo.getNodeName());
+                System.out.println("Percents  "+entry.getValue().getCpu()/nodeInfo.getNode_top_cpu_value()+"  ");
+                System.out.println("Estimate  "+(nodeInfo.getNode_cpu_total()-nodeInfo.getNode_top_cpu_value())/entry.getValue().getCpu()+"  ");
+                System.out.println("NowNode  "+nodeInfo.getNode_top_cpu_percents()+"  ");
+                System.out.println("NowNode  "+nodeInfo.getNode_load_cpu_percents()+"  ");
+                System.out.println("NowNode  "+nodeInfo.getCpu_idle_percent()+"  ");
             }
         }
     }
