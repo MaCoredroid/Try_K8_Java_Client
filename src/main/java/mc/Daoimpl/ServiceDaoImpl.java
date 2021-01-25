99e653492195407dcc322e9375525b876f9a96bd
macoredroid <coredroid0401@gmail.com>
2021-01-25 20:03:40 +0800

    update

--- src/main/java/mc/Daoimpl/ServiceDaoImpl.java
+++ src/main/java/mc/Daoimpl/ServiceDaoImpl.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.context.WebApplicationContext;
 
-import javax.jnlp.ServiceManager;
 import java.util.HashMap;
 
 @Repository
