1a2a99268f3b813807b4e2bea055aaef729ae660
macoredroid <coredroid0401@gmail.com>
2021-01-26 17:23:35 +0800

    update

--- src/main/java/mc/App.java
+++ src/main/java/mc/App.java
@@ -11,27 +11,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
-import io.kubernetes.client.openapi.apis.CoreV1Api;
-import mc.Component.KubernetesApiClient;
-import mc.DTO.NodeInfo;
-import mc.Task.CheckNodeList;
-import mc.Task.CheckNodeStatus;
-import mc.Task.CheckPodStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Timer;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 /**
  * A simple example of how to use the Java API inside a kubernetes cluster
@@ -48,41 +35,41 @@ public class App {
     WebApplicationContext applicationContext;
     public static void main(String[] args) throws IOException, ApiException {
 
-        ConfigurableApplicationContext run=SpringApplication.run(App.class, args);
-        App app = run.getBean(App.class);
-        app.run();
-        
+//        ConfigurableApplicationContext run=SpringApplication.run(App.class, args);
+//        App app = run.getBean(App.class);
+//        app.run();
+        SpringApplication.run(App.class, args);
     }
-    private void run(){
-        Runnable r1 = () -> {
-            CoreV1Api api=applicationContext.getBean(KubernetesApiClient.class).getAPI();
-            ApiClient client=applicationContext.getBean(KubernetesApiClient.class).getClient();
-            HashMap<String, NodeInfo> nodeMap=new HashMap<>();
-            HashMap<String, String> nodeNameToIP=new HashMap<>();
-//        // invokes the CoreV1Api client
-            Timer t = new Timer();
-            CheckNodeStatus checkNodeStatus =new CheckNodeStatus(api,nodeMap);
-            CheckNodeList checkNodeList=new CheckNodeList(api,nodeMap,nodeNameToIP);
-            CheckPodStatus checkPodStatus=new CheckPodStatus();
-//            CheckPodAndNodeUsage checkPodAndNodeUsage =new CheckPodAndNodeUsage(client,nodeMap,serviceNameMap,nodeNameToIP);
-//            Calculate calculate=new Calculate(serviceNameMap, nodeMap);
-//            t.scheduleAtFixedRate(checkNodeStatus, 0, 500);
-//            t.scheduleAtFixedRate(checkPodAndNodeUsage, 0, 500);
-//            t.scheduleAtFixedRate(checkNodeList, 0, 5000);
-//            t.scheduleAtFixedRate(checkPodStatus, 0, 5000);
-//            t.scheduleAtFixedRate(calculate, 0, 1000);
-        };
-
-        //Create an executor service with 2 threads (it can be like 50
-        //if you need it to be).  Submit our two tasks to it and they'll
-        //both run to completion (or forever if they don't end).
-        ExecutorService service = Executors.newFixedThreadPool(2);
-        service.submit(r1);
-
-        //Wait or completion of tasks (or forever).
-        service.shutdown();
-        try { service.awaitTermination(Integer.MAX_VALUE, TimeUnit.DAYS); }
-        catch (InterruptedException e) { e.printStackTrace(); }
-    }
+//    private void run(){
+//        Runnable r1 = () -> {
+//            CoreV1Api api=applicationContext.getBean(KubernetesApiClient.class).getAPI();
+//            ApiClient client=applicationContext.getBean(KubernetesApiClient.class).getClient();
+//            HashMap<String, NodeInfo> nodeMap=new HashMap<>();
+//            HashMap<String, String> nodeNameToIP=new HashMap<>();
+////        // invokes the CoreV1Api client
+//            Timer t = new Timer();
+//            CheckNodeStatus checkNodeStatus =new CheckNodeStatus(api,nodeMap);
+//            CheckNodeList checkNodeList=new CheckNodeList(api,nodeMap,nodeNameToIP);
+//            CheckPodStatus checkPodStatus=new CheckPodStatus();
+////            CheckPodAndNodeUsage checkPodAndNodeUsage =new CheckPodAndNodeUsage(client,nodeMap,serviceNameMap,nodeNameToIP);
+////            Calculate calculate=new Calculate(serviceNameMap, nodeMap);
+////            t.scheduleAtFixedRate(checkNodeStatus, 0, 500);
+////            t.scheduleAtFixedRate(checkPodAndNodeUsage, 0, 500);
+////            t.scheduleAtFixedRate(checkNodeList, 0, 5000);
+////            t.scheduleAtFixedRate(checkPodStatus, 0, 5000);
+////            t.scheduleAtFixedRate(calculate, 0, 1000);
+//        };
+//
+//        //Create an executor service with 2 threads (it can be like 50
+//        //if you need it to be).  Submit our two tasks to it and they'll
+//        //both run to completion (or forever if they don't end).
+//        ExecutorService service = Executors.newFixedThreadPool(2);
+//        service.submit(r1);
+//
+//        //Wait or completion of tasks (or forever).
+//        service.shutdown();
+//        try { service.awaitTermination(Integer.MAX_VALUE, TimeUnit.DAYS); }
+//        catch (InterruptedException e) { e.printStackTrace(); }
+//    }
 
 }
--- src/main/java/mc/Task/Calculate.java
+++ src/main/java/mc/Task/Calculate.java
@@ -1,40 +1,40 @@
-package mc.Task;
-
-import mc.DTO.*;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TimerTask;
-public class Calculate extends TimerTask {
-    HashMap<String, ServiceInfo> serviceNameMap = new HashMap<>();
-    HashMap<String, NodeInfo> nodeMap=new HashMap<>();
-    public Calculate(HashMap<String, ServiceInfo> serviceNameMap,HashMap<String, NodeInfo> nodeMap)
-    {
-        this.serviceNameMap=serviceNameMap;
-        this.nodeMap=nodeMap;
-    }
-    @Override
-    public void run() {
-        ExecutionDTO executionDTO=new ExecutionDTO();
-        executionDTO.setServiceIP(serviceNameMap.getOrDefault("application", new ServiceInfo()).getClusterIP());
-
-        for(Map.Entry<String, PodInfo> entry:serviceNameMap.getOrDefault("application", new ServiceInfo()).getPods().entrySet())
-        {
-            String nodeIP=entry.getValue().getNodeIP();
-//            System.out.println(entry.getValue().getPodName()+" ");
-            if(nodeMap.containsKey(nodeIP))
-            {
-                NodeInfo nodeInfo=nodeMap.get(nodeIP);
-                executionDTO.getExecutionDetailDTOS().add(new ExecutionDetailDTO(entry.getValue().getPodIP(),0));
-//                System.out.println("Node "+nodeInfo.getNodeName());
-//                System.out.println("Percents  "+entry.getValue().getCpu()/nodeInfo.getNode_top_cpu_value()+"  ");
-//                System.out.println("Estimate  "+(nodeInfo.getNode_cpu_total()-nodeInfo.getNode_top_cpu_value())/entry.getValue().getCpu()+"  ");
-//                System.out.println("NowNode  "+nodeInfo.getNode_top_cpu_percents()+"  ");
-//                System.out.println("NowNode  "+nodeInfo.getNode_load_cpu_percents()+"  ");
-//                System.out.println("NowNode  "+nodeInfo.getCpu_idle_percent()+"\n");
-            }
-        }
-//        Execution execution=new Execution(nodeMap,executionDTO);
-//        execution.run();
-    }
-}
+//package mc.Task;
+//
+//import mc.DTO.*;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.TimerTask;
+//public class Calculate extends TimerTask {
+//    HashMap<String, ServiceInfo> serviceNameMap = new HashMap<>();
+//    HashMap<String, NodeInfo> nodeMap=new HashMap<>();
+//    public Calculate(HashMap<String, ServiceInfo> serviceNameMap,HashMap<String, NodeInfo> nodeMap)
+//    {
+//        this.serviceNameMap=serviceNameMap;
+//        this.nodeMap=nodeMap;
+//    }
+//    @Override
+//    public void run() {
+//        ExecutionDTO executionDTO=new ExecutionDTO();
+//        executionDTO.setServiceIP(serviceNameMap.getOrDefault("application", new ServiceInfo()).getClusterIP());
+//
+//        for(Map.Entry<String, PodInfo> entry:serviceNameMap.getOrDefault("application", new ServiceInfo()).getPods().entrySet())
+//        {
+//            String nodeIP=entry.getValue().getNodeIP();
+////            System.out.println(entry.getValue().getPodName()+" ");
+//            if(nodeMap.containsKey(nodeIP))
+//            {
+//                NodeInfo nodeInfo=nodeMap.get(nodeIP);
+//                executionDTO.getExecutionDetailDTOS().add(new ExecutionDetailDTO(entry.getValue().getPodIP(),0));
+////                System.out.println("Node "+nodeInfo.getNodeName());
+////                System.out.println("Percents  "+entry.getValue().getCpu()/nodeInfo.getNode_top_cpu_value()+"  ");
+////                System.out.println("Estimate  "+(nodeInfo.getNode_cpu_total()-nodeInfo.getNode_top_cpu_value())/entry.getValue().getCpu()+"  ");
+////                System.out.println("NowNode  "+nodeInfo.getNode_top_cpu_percents()+"  ");
+////                System.out.println("NowNode  "+nodeInfo.getNode_load_cpu_percents()+"  ");
+////                System.out.println("NowNode  "+nodeInfo.getCpu_idle_percent()+"\n");
+//            }
+//        }
+////        Execution execution=new Execution(nodeMap,executionDTO);
+////        execution.run();
+//    }
+//}
